f52648596405c95f74823b1ea6ba93989d0a8056
Moses Jingo <jingomoses@gmail.com>
2023-02-19 11:56:10 -0800

    prints & follows by a new line, to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+* main - main function initialised
+* Description: ' prints & follows by a new line, to the standard error.
+* Return: Always 1 (Error)
+*/
+int main(void)
+{
+write(1, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+return (1);
+}
