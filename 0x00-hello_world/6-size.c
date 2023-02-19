0b2c1fda78a0ca3dd452f5602ca1a6ea01f562c3
Moses Jingo <jingomoses@gmail.com>
2023-02-19 10:19:22 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main function initialised
+ * Description: 'produce the exact same output as in the example
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
+return (0);
+}
