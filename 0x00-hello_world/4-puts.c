e84cd1855db55bc0dab98ad5837d8ca019c21d73
Moses Jingo <jingomoses@gmail.com>
2023-02-19 06:50:17 -0800

    my first C test

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - main funtion initialised
+ * Description: 'my first C program'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char(msg) = "Programming is like building a multilingual puzzle";
+puts(msg);
+return (0);
+}
