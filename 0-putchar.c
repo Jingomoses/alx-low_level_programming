dd8a7c990aa46f78807aaae9a58e610d37eed59e
Moses Jingo <jingomoses@gmail.com>
2023-02-20 23:41:46 -0800

    prints _putchar, followed by a new line.

---
+++ 0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - main function initialised
+* Description: 'prints _putchar, followed by a new line.'
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
