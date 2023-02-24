8cfa5eee182c0c26f8db9717d77324f144b00fa9
Moses Jingo <jingomoses@gmail.com>
2023-02-24 01:11:49 -0800

    adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* add - adds two integers and returns the result
+* @a: number to be checked
+* @b: number to be checked
+* Return: 1 if true, 0 if false
+*/
+
+int add(int a, int b)
+{
+return (a + b);
+}
